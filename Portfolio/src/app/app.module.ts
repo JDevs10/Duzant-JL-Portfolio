53cab8abe4c5e4eeddc9dbdb50eaf96ee040be7e
Jean-Laurent Duzant <j-ldsxm18@hotmail.com>
2018-12-19 10:25:01 +0100

    ---------- JL Portfolio Angular ----------
    - first commit
    - add header, footer, contact components
    - styled the components

---
+++ Portfolio/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HttpClientModule }    from '@angular/common/http';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
