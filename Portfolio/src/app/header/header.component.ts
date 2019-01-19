a4320caa9394235ba0db3be18a11d92834866da5
Jean-Laurent Duzant <j-ldsxm18@hotmail.com>
2019-01-19 23:39:44 +0100

    navBar is fixed; smooth slide is active

--- Portfolio/src/app/header/header.component.ts
+++ Portfolio/src/app/header/header.component.ts
@@ -21,4 +21,31 @@ export class HeaderComponent implements OnInit {
     }
   }
 
+  currentActive(screenView: String){
+    var w = [];
+    w[0] = document.getElementById("navHome").className;
+    w[1] = document.getElementById("navSkills").className;
+    w[2] = document.getElementById("navEducation").className;
+    w[3] = document.getElementById("navProjects").className;
+    w[4] = document.getElementById("navContact").className;
+
+    var x = ["navHome", "navSkills", "navEducation", "navProjects", "navContact"];
+
+    for(var y = 0; y < w.length; y++){
+      // console.log("x["+y+"] = "+x[y]);
+      if(w[y] == "nav-item nav-link active"){
+        document.getElementById(x[y]).className = "nav-item nav-link";
+        // console.log("getElementById("+x[y]+").className =  nav-item nav-link");
+      }
+    }
+    
+    var z = document.getElementById("nav"+screenView);
+    if(z.className == "nav-item nav-link"){
+      z.className += " active";
+    }else{
+      z.className = "nav-item nav-link";
+    }
+    //console.log("nav id: nav"+screenView+"\n"+y.className.toString());
+  }
+
 }
