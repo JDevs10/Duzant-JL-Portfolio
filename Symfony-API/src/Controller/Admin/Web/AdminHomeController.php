abef78dac90d36998247a3431d589a3a70826620
Jean-Laurent Duzant <j-ldsxm18@hotmail.com>
2019-02-03 06:24:04 +0100

    delete unsellable files; the home list is done

--- Symfony-API/src/Controller/Admin/Web/AdminHomeController.php
+++ Symfony-API/src/Controller/Admin/Web/AdminHomeController.php
@@ -1,13 +1,19 @@
 <?php
 namespace App\Controller\Admin\Web;
 
-use Symfony\Component\HttpFoundation\JsonResponse;
+// use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\RedirectResponse;
+// use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+
+use App\Entity\Bodies;
+use App\Entity\Skills;
+use App\Entity\Schools;
+use App\Entity\Jobs;
+use App\Entity\Work;
 
 /**
  * @Route("/admin")
@@ -19,8 +25,33 @@ class AdminHomeController extends AbstractController{
      */
     public function home(Request $request){
         // show tables of each database table data
+        // home data
+        $repository = $this->getDoctrine()->getRepository(Bodies::class);
+        $bodies = $repository->findAll();
 
-        return $this->render('Admin/home.html.twig');
+        // skill data
+        $repository = $this->getDoctrine()->getRepository(Skills::class);
+        $skills = $repository->findAll();
+
+        // education data
+        $repository = $this->getDoctrine()->getRepository(Schools::class);
+        $schools = $repository->findAll();
+
+        // job data
+        $repository = $this->getDoctrine()->getRepository(Jobs::class);
+        $jobs = $repository->findAll();
+
+        // projects data
+        $repository = $this->getDoctrine()->getRepository(Work::class);
+        $works = $repository->findAll();
+
+        return $this->render('Admin/home.html.twig', array(
+            "bodies" => $bodies,
+            "skills" => $skills,
+            "schools" => $schools,
+            "jobs" => $jobs,
+            "works" => $works
+        ));
     }
         
 }
